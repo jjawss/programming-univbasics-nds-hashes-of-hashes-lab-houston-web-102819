82b6798d3c7d83ee256b428501e0eb70221c96bd
David Jaworski <jaws360@gmail.com>
2019-10-24 00:21:49 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,6 +49,10 @@ def naming_system
           label: "Class",
           sub_category: {
             label: "Order"
+              label: "Family",
+                sub_category: {
+                  label: "Genus",
+            }
           }
         }
       }
